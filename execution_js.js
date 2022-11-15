c42d400130f4763c24a1b32517ac704ae03ea07d
Harshit Raj <41303856+harshit-8080@users.noreply.github.com>
2022-11-15 21:34:28 +0530

    scopes and intro  to js

---
+++ execution_js.js
@@ -0,0 +1,14 @@
+console.log(name);
+
+console.log(age);
+
+var name = "harshit";
+
+var age = 25;
+
+console.log(name);
+
+console.log(age);
+
+x = 100;
+console.log(x);
---
+++ scopes.js
@@ -0,0 +1,6 @@
+const x = 100;
+
+x = "kk";
+
+
+console.log(x);
