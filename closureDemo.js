28c234a20ab52e120b72c314d3b8c2b77051de6c
Harshit Raj <41303856+harshit-8080@users.noreply.github.com>
2022-11-19 17:08:05 +0530

    closures and setTimeout

---
+++ closureDemo.js
@@ -0,0 +1,17 @@
+
+
+
+console.log("start");
+
+for(let i = 1; i<=5; i++){
+
+
+    setTimeout(function fun() {
+    
+       console.log(i);
+    
+    },1000);
+
+}
+
+console.log("end");
--- setTimeout_closures.js
+++ setTimeout_closures.js
@@ -3,10 +3,20 @@ console.log("start");
 
 setTimeout(function fun() {
     
-    console.log("setTimeout ====>>> ");
+    console.log("setTimeout 1 sec ====>>> ");
+
+},1000);
+
+setTimeout(function fun() {
+    
+    console.log("setTimeout 2 sec ====>>> ");
 
 },2000);
 
+setTimeout(function fun() {
+    
+    console.log("setTimeout 3 sec ====>>> ");
 
+},3000);
 
 console.log("end");
